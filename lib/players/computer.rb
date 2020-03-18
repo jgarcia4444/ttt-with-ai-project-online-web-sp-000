1c84e3a27422532ae26693113252a14e62a22789
Jake Garcia <bballwiz4@gmail.com>
2020-03-18 19:15:26 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,6 +5,9 @@ module Players
     def move(board)
       valid_move_array = []
       board.each_with_index do |ele, i|
+        i_move = i + 1
+        if board.valid_move?(i_move)
+        end
       end
     end
 
